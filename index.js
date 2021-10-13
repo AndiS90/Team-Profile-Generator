5cde8967b8e0a28053f5c2adec302e51934bb2a9
andi <andrea.strickland1990@gmail.com>
2021-10-13 17:12:13 -0500

    finally got program to run. still struggling properly generating the html. it's generating, but not properly

--- index.js
+++ index.js
@@ -1,5 +1,187 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
-const manager = require("./lib/Manager");
-const engineer = require("./lib/Engineer");
-const intern = require("./lib/Intern");
+const employee = require("./lib/employee.js")
+const manager = require("./lib/manager.js");
+const engineer = require("./lib/engineer.js");
+const intern = require("./lib/intern.js");
+const genHTML = require("./src/generateHTML");
+
+let allEngineers = [];
+let allInterns = [];
+let allManagers = [];
+
+let title = "";
+
+let menuAnswer = "";
+
+const team = [{
+    type: 'input',
+    name: 'team',
+    message: 'What is your team name?',
+}];
+
+const menu = [{
+    type: "list",
+    name: "menu",
+    message: "You may add:",
+    choices: ["Engineer", "Intern", "Finish!"]
+}];
+
+const managerQs = [{
+        type: 'input',
+        name: 'moniker',
+        message: 'What is the manager name?',
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: 'What is the manager ID?',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is the manager email?',
+    },
+    {
+        type: 'input',
+        name: 'office',
+        message: 'What is the manager office number?',
+    }
+];
+
+const engineerQs = [{
+        type: 'input',
+        name: 'moniker',
+        message: 'What is the engineer name?',
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: 'What is the engineer ID?',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is the engineer email?',
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is the engineer GitHub Username? (case matters):',
+    }
+];
+
+const internQs = [{
+        type: 'input',
+        name: 'moniker',
+        message: 'What is the intern name?',
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: 'What is the intern ID?',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is the intern email?',
+    },
+    {
+        type: 'input',
+        name: 'school',
+        message: 'Where did the intern go to school?',
+    }
+];
+async function addEng(){
+
+    await inquirer.prompt(engineerQs)
+    .then((data) => {
+
+        let b = new engineer(data.id, data.moniker, data.email, data.github);
+
+        allEngineers.push(b);
+})};
+
+async function addInt(){
+     await inquirer.prompt(internQs)
+     .then((data) => {
+
+        let c = new intern(data.id, data.moniker, data.email, data.school);
+
+        allInterns.push(c);
+    })
+};
+
+async function addteam(){
+
+    await  inquirer.prompt(team) 
+    .then((data) => {
+        title = data.team; 
+        
+        writeToFile(title, allManagers, allEngineers, allInterns);
+    })
+};
+
+
+async function promptUser() {
+
+  await  inquirer.prompt(managerQs)
+        .then((data) => { 
+
+            let a = new manager(data.id, data.moniker, data.email, data.office)
+
+            allManagers.push(a);
+        })
+    promptMenu();
+
+};
+
+async function promptMenu()  {
+    await inquirer.prompt(menu)
+        .then((data) => { menuAnswer = data.menu;
+             menuIf(menuAnswer);})
+
+async function menuIf(answer){
+
+            if (answer === "Engineer") {
+
+                   await addEng()   
+                    
+                    
+
+                promptMenu();
+
+            } else if (answer === "Intern") {
+
+                await addInt()
+                    
+
+                promptMenu();
+
+            } else if (answer === "Finish!") {
+
+                   await addteam()
+                  
+
+               
+
+                //return;
+
+            }
+        }
+};
+
+
+
+// TODO: Create a function to write README file
+function writeToFile(title, managers, engineers, interns) {
+    const htmlContent = genHTML.generateHTML(title, managers, engineers, interns);
+
+    fs.writeFile(`${title}.html`, htmlContent, (err) =>
+        err ? console.log(err) : console.log(`Successfully created a new team Profile at ${title}.html!`)
+    );
+};
+
+// TODO: Create a function to initialize app
+
+promptUser();
--- src/generateHTML.js
+++ src/generateHTML.js
@@ -1,23 +1,23 @@
 //TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderManager(name, id, email, office) {
-  if ( !== 'None') {
-    return `<div class="col s12 m4 l3">
+function renderManager(managers) {
+  if (managers) {
+    managers.forEach(manager => `<div class="col s12 m4 l3">
     <div class="card">
         <div class="card-content top">
-            <h2 class="card-title "><strong>${name}</strong></h2>
+            <h2 class="card-title "><strong>${manager.moniker}</strong></h2>
             <h3 class=" valign-wrapper ">Manager &nbsp; <i class="small material-icons"> brightness_5</i>
             </h3>
         </div>
         <div class="card-action bottom">
             <ul class="">
-                <li class="">ID: ${id}</li>
-                <li class="">EMAIL: <a href="mailto:${email}"> ${email} </a></li>
-                <li class="">OFFICE: ${office}</li>
+                <li class="">ID: ${manager.id}</li>
+                <li class="">EMAIL: <a href="mailto:${manager.email}"> ${manager.email} </a></li>
+                <li class="">OFFICE: ${manager.office}</li>
             </ul>
         </div>                  
     </div>
-</div>`;
+</div>` );
   }
   return '';
 
@@ -25,51 +25,52 @@ function renderManager(name, id, email, office) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderEngineer(name, id, email, github) {
-  if ( !== 'None') {
-    return `<div class="col s12 m4 l3">
+function renderEngineers(engineers) {
+  if ( engineers.length !== 0) {
+
+    engineers.forEach(engineer => `<div class="col s12 m4 l3">
     <div class="card">
         <div class="card-content top">
-            <h2 class="card-title"><strong>${name}</strong></h2>
+            <h2 class="card-title"><strong>${engineer.moniker}</strong></h2>
             <h3 class=" valign-wrapper">Engineer &nbsp;<i class="small material-icons"> brightness_3</i></h3>
         </div>
         <div class="card-action bottom">
             <ul class="">
-                <li class="">ID: ${id} </li>
-                <li class="">EMAIL:<a href="mailto:${email}">${email}</a> </li>
-                <li class="">GITHUB:<a href="https://github.com/${github}"> ${github}</a></li>
+                <li class="">ID: ${engineer.id} </li>
+                <li class="">EMAIL:<a href="mailto:${engineer.email}">${engineer.email}</a> </li>
+                <li class="">GITHUB:<a href="https://github.com/${engineer.github}"> ${engineer.github}</a></li>
             </ul>
         </div>
     </div>
-</div>`;
+</div>` );
   }
   return '';
 }
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderIntern(name, id, email, school) {
-  if (!== 'None') {
-    return `<div class="col s12 m4 l3">
+function renderInterns(interns) {
+  if ( interns.length !== 0) {
+    interns.forEach(intern => `<div class="col s12 m4 l3">
     <div class="card ">
         <div class="card-content top">
-            <h2 class="card-title"><strong>${name}</strong></h2>
+            <h2 class="card-title"><strong>${intern.moniker}</strong></h2>
             <h3 class="valign-wrapper">Intern &nbsp; <i class="small material-icons"> cloud</i> </h3>
         </div>
         <div class="card-action bottom">
             <ul class="">
-                <li class="">ID: ${id}</li>
-                <li class="">EMAIL:<a href="mailto:${email} ">${email}</a></li>
-                <li class="">SCHOOL: ${school}</li>
+                <li class="">ID: ${intern.id}</li>
+                <li class="">EMAIL:<a href="mailto:${intern.email} ">${intern.email}</a></li>
+                <li class="">SCHOOL: ${intern.school}</li>
             </ul>
         </div>
     </div>
-</div>`;
+</div>` );
   }
   return '';
 
 };
 // TODO: Create a function to generate markdown for README
-function generateHTML(data) {
+function generateHTML(title, managers, engineers, interns) {
   return `<!DOCTYPE html>
   <html lang="en">
   
@@ -77,7 +78,7 @@ function generateHTML(data) {
       <meta charset="UTF-8">
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-      <title>${data.team}</title>
+      <title>${title}</title>
   
   
       <!-- Compiled and minified CSS -->
@@ -85,25 +86,29 @@ function generateHTML(data) {
   
       <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
   
-      <link rel="stylesheet" href="./style.css">
+      <link rel="stylesheet" href="./dist/style.css">
   </head>
   
   <style>
       body {
-          background-image: url('../assets/images/wallpaper-1.jpg');
+          background-image: url('./assets/images/wallpaper-1.jpg');
       }
   </style>
   
   <body>
       <nav>
           <div class="nav-wrapper">
-              <h1 class="brand-logo center">My Team</h1>
+              <h1 class="brand-logo center">${title}</h1>
           </div>
       </nav>
       <div class=" container ">
           <div class="row">
           
+          ${renderManager(managers)}
+
+          ${renderEngineers(engineers)}
           
+          ${renderInterns(interns)}
           
           
           
